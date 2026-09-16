3fefe42d9aa24b453b0f96e31186ef0716da3aa8
Anvesha Singh <singhanvesha545@gmail.com>
2026-09-16 23:19:15 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = prices[0];
+        int profit =0;
+        int n = prices.size();
+
+        for(int i=1;i<n;i++){
+            if(prices[i] < buy){
+                buy = prices[i];
+            }
+        
+            int maxprofit = prices[i] - buy ;
+        
+    
+            if(maxprofit > profit){
+                profit = maxprofit;
+            
+            }
+        }
+        return profit;
+    }
+};
